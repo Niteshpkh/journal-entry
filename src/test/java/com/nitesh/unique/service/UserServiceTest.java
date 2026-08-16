b88b8787f84353fcd1886fe780410d8a4209d803
Nitesh Pokhrel <niteshpokhrel84@gmail.com>
2026-08-16 08:29:46 +0545

    Simple Junit test

---
+++ src/test/java/com/nitesh/unique/service/UserServiceTest.java
@@ -0,0 +1,28 @@
+package com.nitesh.unique.service;
+
+import com.nitesh.unique.repository.UserRepository;
+import com.nitesh.unique.services.UserService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Test
+    public  void testFindByUserName(){
+        assertNotNull(userRepository.findByUserName("anmol"));
+    }
+
+}
