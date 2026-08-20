8e6864890fed9e2b767378d9bc9c7a605df6d385
Nitesh Pokhrel <niteshpokhrel84@gmail.com>
2026-08-20 21:09:00 +0545

    Test simple external apis

---
+++ src/main/java/com/nitesh/unique/services/QuoteService.java
@@ -0,0 +1,5 @@
+package com.nitesh.unique.services;
+
+public class QuoteService {
+    private static final String api_key = "lmb7jBPFzGIwJTzvbQskpFikyQINnf9Rq4CS2tGS";
+}
---
+++ src/main/java/com/nitesh/unique/services/WeatherService.java
@@ -0,0 +1,5 @@
+package com.nitesh.unique.services;
+
+public class WeatherService {
+    private static final String api_key = "3c8ebc8e889b8963d562f27b32477568";
+}
